81eccd35e3aa54c018f3da262f9164798c343763
Ramesh Yadav <113420314+rameshy9891@users.noreply.github.com>
2023-10-31 05:27:40 +0530

    practice of stream api

---
+++ Day_1/src/com/hard/challenge/tenquestion/oneday/ZzzzStreamApiUse.java
@@ -0,0 +1,34 @@
+package com.hard.challenge.tenquestion.oneday;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ZzzzStreamApiUse {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> intList= Arrays.asList(10,20,30);
+		
+		//System.out.println(intList);
+		
+		System.out.println(intList.stream().filter(i->i*i==100).collect(Collectors.toSet()));
+		
+		Integer stream=intList.stream().reduce(0, (i1,i2)->i1+i2);
+		
+		System.out.println(stream);
+		
+		System.out.println(intList.stream().reduce(1, (i,j)-> i*j));
+		
+		System.out.println(intList.stream().collect(Collectors.reducing(0, (n1,n2)->n1+n2)));
+		
+		Optional<Integer> max=	intList.stream().collect(Collectors.maxBy(Integer::compare));
+		
+		
+		
+		
+		System.out.println(max.get());
+		
+	}
+}
